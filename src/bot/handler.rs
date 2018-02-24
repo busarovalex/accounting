f2af1dd00208e834eed3ce3fe8e1f4f7f6ccafc9
Alex <busarovalex@yandex.ru>
2018-02-24 23:00:29 +0300

    fix

--- src/bot/handler.rs
+++ src/bot/handler.rs
@@ -16,11 +16,12 @@ pub fn handle(data: &str, config: &Config, registry: &Registry, user: UserId) ->
             "отчет" | "/отчет" | "Отчет" => {
                 super::report::report(&mut words, config, registry, user)                
             },
-            query @ _ => {
-                let parsed_new_product = Product::from_str(&query)?;
+            _ => {
+                let parsed_new_product = Product::from_str(&data)?;
                 let new_entry = Entry::new(user, parsed_new_product);
+                let response = added_entry(registry, &new_entry)?;
                 registry.add_entry(new_entry)?;
-                Ok(format!("Ok"))
+                Ok(response)
             }
         }
     } else {
@@ -30,4 +31,13 @@ pub fn handle(data: &str, config: &Config, registry: &Registry, user: UserId) ->
 
 fn help() -> String {
     format!("/help\n/отчет")
+}
+
+fn added_entry(registry: &Registry, entry: &Entry) -> Result<String, Error> {
+    let categories = registry.categories(entry.user_id.clone())?;
+    if let Some(category) = categories.iter().find(|c| c.product == entry.product.name) {
+        Ok(format!("{} ({}) - {} руб.", entry.product.name, category.category, entry.product.price))
+    } else {
+        Ok(format!("{} - {} руб.", entry.product.name, entry.product.price))
+    }
 }
